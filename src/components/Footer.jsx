e04519e08927552cc81d3439c0fb8efe7788d4c9
MattBarnabo <mattbarnabo@gmail.com>
2024-04-03 17:16:23 -0500

    adds styling to header, footer, and not found page

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { NavLink } from "react-router-dom"
+import pmlSmall from "../assets/Logos/pmlsmall.png"
 
 const Footer = () => {
   return (
-    <div>
-      
-    </div>
+    <footer>
+      <div className='footer-content'>
+        <p>&copy; 2024 Seth & Matt</p>
+        <NavLink to='/'>
+          <img
+          src= {pmlSmall}
+          alt='logo of cat with heart in a circle'
+          className='cat-logo-small'
+          />
+        </NavLink>
+      </div>
+    </footer>
   )
 }
 
