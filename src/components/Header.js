3efc2db693668de86f83d5e4ed26227d408bb6fa
MattBarnabo <mattbarnabo@gmail.com>
2024-04-03 17:52:46 -0500

    adds formating fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,27 @@
-import React from 'react';
-import { Nav } from 'reactstrap'
+import React from "react";
+import { Nav } from "reactstrap"
 import { NavLink } from "react-router-dom"
 import Logo from "./../assets/Logos/pmlLarge.png"
 
-
-
 const Header = () => {
-
-
-
   return (
     <Nav className="me-auto">
       <div>
         <NavLink to="/">
           <img
             src= {Logo}
-            alt='logo of a cat with a heart in a circle and the name perrfect match adopt a cat below'
-            className='perrfect-match-logo'
+            alt="logo of a cat with a heart in a circle and the name perrfect match adopt a cat below"
+            className="perrfect-match-logo"
           />
           </NavLink>
       </div>
-      <div className='header-nav-link'>
+      <div className="header-nav-link">
         <NavLink to="/cat-new" className="nav-link" >
           Add a Cat
         </NavLink>
         <a
-        href='https://purrfectmatchcatrescue.org/donate?gad_source=1&gclid=CjwKCAjw_LOwBhBFEiwAmSEQAX3HmXgZ5a_mRYW6YEAA4mYh44P2QuiMWTrwcTPVgpTZUHPIvPuP8hoCtswQAvD_Bw'
-        target='blank'
+        href="https://purrfectmatchcatrescue.org/donate?gad_source=1&gclid=CjwKCAjw_LOwBhBFEiwAmSEQAX3HmXgZ5a_mRYW6YEAA4mYh44P2QuiMWTrwcTPVgpTZUHPIvPuP8hoCtswQAvD_Bw"
+        target="blank"
         className="nav-link"
         >
           Adopt a Cat
--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NotFound = () => {
   let randomNumber = Math.ceil(Math.random()*3)
-  const Message = () => {
+  const message = () => {
     const message1 = "404: Oops! Looks like this cool cat  has wandered off with the page you're seeking!"
     const message2 = "404: The cool cat you're looking for is currently sunbathing elsewhere."
     const message3 = "404: This page is on a catnap. Let's find another spot to lounge!"
@@ -14,7 +14,7 @@ const NotFound = () => {
         return message3
       }
   }
-  const Image = () => {
+  const image = () => {
     const image1 = "https://images.unsplash.com/photo-1533738363-b7f9aef128ce?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     const image2 = "https://plus.unsplash.com/premium_photo-1677545183884-421157b2da02?q=80&w=2072&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
     const image3 = "https://images.unsplash.com/photo-1549317909-68b039b635e7?q=80&w=2080&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
@@ -29,15 +29,15 @@ const NotFound = () => {
 
   return (
     <>
-      <div className='not-found-content'>
+      <div className="not-found-content">
         <div>
-          <img src={Image()}
-          alt='cat'
-          className='image'
+          <img src={image()}
+          alt="cat"
+          className="image"
           />
         </div>
-        <div className='not-found-title'>
-            <h3 className='message'>{Message()}</h3>
+        <div className="not-found-title">
+            <h3 className="message">{message()}</h3>
         </div>
       </div>
     </>
