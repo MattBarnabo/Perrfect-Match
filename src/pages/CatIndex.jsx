43d0c24b4151930d0542a9e01e5ca66ba1dbd738
MattBarnabo <mattbarnabo@gmail.com>
2024-04-04 13:37:53 -0500

    adds cats from mockCats and cards along with passing tests

--- src/pages/CatIndex.jsx
+++ src/pages/CatIndex.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import React from "react"
+import { Card, CardBody, CardTitle, } from "reactstrap"
+import NavButton from "../components/NavButton"
 
-const CatIndex = () => {
+const CatIndex = ({ cats }) => {
   return (
-    <div>
-      <h3>Cat Index</h3>
+    <div className="page-body">
+      <h2 className="page-title">Meet your Perrfect Match</h2>
+      <div className="index-cat-cards">
+        {cats.map((cat) => {
+          return(
+            <Card key={cat.id}>
+              <div className="index-profile-image-container">
+                <img
+                  alt={`profile of a cat named ${cat.name}`}
+                  src={cat.image}
+                  className="index-profile-image"
+              />
+            </div>
+            <CardBody>
+              <CardTitle className="index-card-text" tag="h5">{cat.name}</CardTitle>
+              <NavButton
+              url={`/cat-show/${cat.id}`}
+              buttonContent="See More"
+            />
+          </CardBody>
+        </Card>
+        )
+      })}
+      </div>
     </div>
   )
 }
