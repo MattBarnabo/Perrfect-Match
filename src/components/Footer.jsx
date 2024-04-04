7901823a86169d567549b909b6bee64172b1ab58
MattBarnabo <mattbarnabo@gmail.com>
2024-04-04 08:17:48 -0500

    adds formatting to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,17 +1,17 @@
-import React from 'react'
+import React from "react"
 import { NavLink } from "react-router-dom"
 import pmlSmall from "../assets/Logos/pmlsmall.png"
 
 const Footer = () => {
   return (
     <footer>
-      <div className='footer-content'>
+      <div className="footer-content">
         <p>&copy; 2024 Seth & Matt</p>
         <NavLink to='/'>
           <img
           src= {pmlSmall}
-          alt='logo of cat with heart in a circle'
-          className='cat-logo-small'
+          alt="logo of cat with heart in a circle"
+          className="cat-logo-small"
           />
         </NavLink>
       </div>
