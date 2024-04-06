7081e4654a46a5e4d41d786866ddd21e868caf1c
MattBarnabo <mattbarnabo@gmail.com>
2024-04-06 15:53:28 -0500

    adds update and delete buttons and functionality

--- src/pages/CatShow.jsx
+++ src/pages/CatShow.jsx
@@ -1,20 +1,47 @@
 import React from "react"
-import { useParams } from "react-router-dom"
+import { useNavigate, useParams } from "react-router-dom"
 import NavButton from "../components/NavButton"
 
-const CatShow = ({ cats }) => {
+const CatShow = ({ cats, deleteCat }) => {
+  const navigate = useNavigate()
   const { id } = useParams()
-  const cat = cats?.find((catObject) => catObject.id === +id)
+  const cat = cats.find((catObject) => catObject.id === +id)
+  const handleDeleteCat = () => {
+    deleteCat(cat.id)
+    navigate("/cat-index")
+  }
+
   return (
     <div className='page-body'>
         <div className="show-profile">
           <ul className="show-text">
-            <li className='page-title'><h2>Meet {cat.name}</h2></li>
-            <li className="show-cat-age"><h4>Age {cat.age}</h4></li>
+            <li className='page-title'>
+              <h2>Meet {cat.name}</h2>
+            </li>
+            <li className="show-cat-age">
+              <h4>Age {cat.age}</h4>
+            </li>
             <li className="show-cat-enjoys">
               <h4>{cat.name} enjoys {cat.enjoys}.</h4>
             </li>
-            <li><NavButton url="/cat-index" buttonContent= "Back to All the Cats"></NavButton></li>
+            <li>
+              <NavButton url={`/cat-edit/${cat.id}`} buttonContent= {`Edit ${cat.name}`}
+              className="show-button"
+              >
+              </NavButton>
+            </li>
+            <li>
+              <button onClick={handleDeleteCat(cat.id)} className="nav-button show-button"
+              >
+                Delete {cat.name}
+              </button>
+            </li>
+            <li>
+              <NavButton url={"/cat-index"} buttonContent= {`Back to all cats`}
+              className="show-button"
+              >
+              </NavButton>
+            </li>
           </ul>
           <img
             src={cat.image}
