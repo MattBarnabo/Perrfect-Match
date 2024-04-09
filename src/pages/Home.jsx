ea319ee0c11b6a7cc0350c622aa62c67b5b3ab24
MattBarnabo <mattbarnabo@gmail.com>
2024-04-09 14:14:12 -0500

    adds styling to home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { NavLink } from "react-router-dom"
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-page">
       <svg className ="curved-text" viewBox="0 0 500 500">
         <path id="curve" d="M73.2,148.6c4-6.1,65.5-96.8,178.6-95.6c111.3,1.2,170.8,90.3,175.1,97" />
         <text width="500">
@@ -14,15 +14,18 @@ const Home = () => {
           </textPath>
         </text>
       </svg>
-      <img src={Logo}
-        alt="logo of a cat with a heart in a circle and the name perrfect match adopt a cat below"
-        className="home-logo"
-      />
-      <NavLink to="/cat-index" className="home-slider">
-        <img src={Slider}
-          alt="slider with pink and white arrow shape with a pink heart"
-        />
-      </NavLink>
+      <div className="home-logo-container">
+        <div className="home-logo">
+          <img src={Logo}
+            alt="logo of a cat with a heart in a circle and the name perrfect match adopt a cat below"
+          />
+          <NavLink to="/cat-index" className="home-slider">
+            <img src={Slider}
+              alt="slider with pink and white arrow shape with a pink heart"
+            />
+          </NavLink>
+        </div>
+      </div>
     </div>
   )
 }
